ff68e34e6dfe413d5227ca555f6e3c8f30a627f3
Your Name <you@example.com>
2025-03-19 03:59:58 +0300

    Add PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// تحقق من دعم Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js") // تأكد من أن المسار صحيح
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+import Logo from "./src/assets/logo-yellow.png"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),
+  plugins: [react(),    tailwindcss(),VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "My PWA App",
+      short_name: "PWA App",
+      start_url: "/",
+      display: "standalone",
+      theme_color: "#000000",
+      background_color: "#ffffff",
+      icons: [
+        {
+          src: Logo,
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+    workbox: {
+      runtimeCaching: [], // لا يتم تخزين أي بيانات محليًا
+    },
+  }),
   ],
 })
