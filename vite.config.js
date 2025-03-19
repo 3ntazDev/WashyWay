fc310600e249c6abcba180271189e2edaa6db4ed
Your Name <you@example.com>
2025-03-19 04:02:17 +0300

    Add PWA 2

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from "vite-plugin-pwa";
-import Logo from "./src/assets/logo-yellow.png"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),VitePWA({
-    registerType: "autoUpdate",
-    manifest: {
-      name: "My PWA App",
-      short_name: "PWA App",
-      start_url: "/",
-      display: "standalone",
-      theme_color: "#000000",
-      background_color: "#ffffff",
-      icons: [
-        {
-          src: Logo,
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "/icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-    workbox: {
-      runtimeCaching: [], // لا يتم تخزين أي بيانات محليًا
-    },
-  }),
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My PWA App",
+        short_name: "PWA App",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/src/assets/logo-yellow.png", // تأكد من أن المسار صحيح
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [], // لا يتم تخزين أي بيانات محليًا
+      },
+    }),
   ],
 })
